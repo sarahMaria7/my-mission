830ddc96e866d755d173660ee2d84eed02c2ee7d
sarahMaria7 <sarouche06@gmail.com>
2021-01-27 20:24:44 +0100

    modified checkPage

--- lib/screens/checkpage.dart
+++ lib/screens/checkpage.dart
@@ -18,51 +18,77 @@ class _CheckPageState extends State<CheckPage> {
         title: Text(''),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 70.0,
-              backgroundImage: AssetImage('assets/images/logoblack.png'),
-            ),
-            new Padding(
-              padding: new EdgeInsets.only(top: 25.0),
-            ),
-            Container(
-                height: 30.0,
-                width: 300.0,
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 95.0,
+                backgroundImage: AssetImage('assets/images/logoblack.png'),
+              ), 
+              SizedBox(height: 52.0), 
+              Padding( 
+                padding: EdgeInsets.all(8.0), 
+              child: Container( 
+                margin: EdgeInsets.only(top: 9.0, bottom: 9.0), 
+                //height: 57.0,
+                //width: 300.0,
                 child: TextField(
-                  textDirection: TextDirection.rtl, 
-                      textAlign: TextAlign.right, 
-                  controller: _checkNum, 
-                       keyboardType: TextInputType.number, 
-                        decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color(0xffFFFFFF),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(9)),
-                        borderSide: BorderSide(
-                          color: Color(0xff333333),
-                          //width: 2.0,
-                        ),
-                      ), 
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(9)),
-                        borderSide: BorderSide(
-                          color: Color(0xffFFFFFF),
-                        ),
-                      ), 
+                  textDirection: TextDirection.rtl,
+                  textAlign: TextAlign.center, 
+                  controller: _checkNum,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xffFFFFFF),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(52)),
+                      borderSide: BorderSide(
+                        color: Color(0xff333333),
+                        width: 2.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(52)),
+                      borderSide: BorderSide(
+                        color: Color(0xff333333), 
+                        width: 2.0, 
+                      ),
+                    ),
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(9)),
-                        borderSide: BorderSide(
-                            //width: 2.0,
-                            ), 
-                    ), 
-                      hintText: (' رمز التأكيد '),
-                    hintStyle: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: Color(0xff333333)), 
-                ),),
+                      borderRadius: BorderRadius.all(Radius.circular(52)),
+                      borderSide: BorderSide(
+                          width: 2.0,
+                          ),
+                    ),
+                    hintText: (' رمز التأكيد '),
+                    hintStyle: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff333333)),
+                  ),
                 ),
-          ],
+              ), 
+              ), 
+              SizedBox(height: 35.0),  
+              Container(
+                child: RaisedButton( 
+                  color: Colors.white12, 
+                  child: Text('ارسال', 
+                  style: new TextStyle(
+                        fontSize: 19.0,
+                        color: Color(0xffFFFFFF),
+                        fontWeight: FontWeight.bold,
+                        backgroundColor: Colors.white12),
+                  ), 
+                   onPressed: (){}, 
+                  ), 
+                   
+                ),
+              
+            ],
+          ),
         ),
       ),
     );
